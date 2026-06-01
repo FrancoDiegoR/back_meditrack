1618f82cba7153c38ad563595ac2eb7cf2d40c9b
Diego Herrera <u202319027@upc.edu.pe>
2026-06-01 12:41:22 -0500

    feat(shared): add new initial migration

---
+++ TechnoByteLambders.MediTrackSensor.Platform/Shared/Infrastructure/Persistence/EFC/Migrations/20260528233120_InitialMigration.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TechnoByteLambders.MediTrackSensor.Platform.Shared.Infrastructure.Persistence.EFC.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySQL:Charset", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
